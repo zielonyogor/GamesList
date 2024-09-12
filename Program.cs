4d7ef3359d082ede4331dae315586cb1ad0319df
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2024-09-12 17:11:27 +0200

    Add Razor Pages

--- Program.cs
+++ Program.cs
@@ -20,6 +20,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 }
 
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
